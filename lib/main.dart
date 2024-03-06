e95b9a4c5cd7c0b7ab787e92c5995967fac6b9e4
Shubh Rai <shubhlusiferrai@gmail.com>
2024-03-06 13:54:22 +0530

    Cupertino minimal app
    Signed-off-by: Shubh Rai <shubhlusiferrai@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:currency_converter/material_home.dart';
+import 'package:currency_converter/cupertino_home.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Mca());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,5 +13,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(home: CcMp());
+  }
+}
+
+class Mca extends StatelessWidget {
+  const Mca({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(home: CcCp());
   }
 }
