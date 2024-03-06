1aecbe9e39c6e6be014ddd73c83ceb5ef1cd8b42
Shubh Rai <shubhlusiferrai@gmail.com>
2024-03-06 13:19:25 +0530

    Material app optimized
    Signed-off-by: Shubh Rai <shubhlusiferrai@gmail.com>

--- lib/material_home.dart
+++ lib/material_home.dart
@@ -37,19 +37,19 @@ class _CcMps extends State<CcMp> {
           titleTextStyle: const TextStyle(color: Colors.white, fontSize: 20),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'INR $result',
-                style: const TextStyle(
-                    fontSize: 45,
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 240, 255, 240)),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'INR ${result != 0 ? result.toStringAsFixed(3) : result.toStringAsFixed(0)}',
+                  style: const TextStyle(
+                      fontSize: 45,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 240, 255, 240)),
+                ),
+                TextField(
                   controller: textEditingController,
                   style: const TextStyle(color: Colors.white),
                   decoration: InputDecoration(
@@ -66,10 +66,10 @@ class _CcMps extends State<CcMp> {
                     decimal: true,
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: ElevatedButton(
+                const SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
                   onPressed: convert,
                   style: ElevatedButton.styleFrom(
                     elevation: 13,
@@ -81,9 +81,9 @@ class _CcMps extends State<CcMp> {
                     ),
                   ),
                   child: const Text('Convert'),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ));
   }
